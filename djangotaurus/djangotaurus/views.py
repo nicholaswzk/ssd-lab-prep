da7136f6bb25078ed3510c55f9ae10fdc91acecc
wongzenkit <1902638@sit.singaporetech.edu.sg>
2021-12-02 10:05:18 +0000

    update

--- djangotaurus/djangotaurus/views.py
+++ djangotaurus/djangotaurus/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
+    if request.method = "POST":
+	user_input = str(request.POST['search'])
+	return render(request,'login.html',{'user_input':user_input});
     return render(request, 'index.html')
 
-
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html',{'user_input':""})
 
 
 def register(request):
