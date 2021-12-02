7f275cb341b4096b7214bbeed81f62f8da1fad84
nicholaswzk <38720664+nicholaswzk@users.noreply.github.com>
2021-12-02 18:34:24 +0800

    Update views.py

--- djangotaurus/djangotaurus/views.py
+++ djangotaurus/djangotaurus/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    if request.method == "POST":
-	user_input = str(request.POST['search'])
-	return render(request,'login.html',{'user_input':user_input});
-    return render(request, 'index.html')
+	if request.method == "POST":
+		user_input = str(request.POST['search'])
+		return render(request,'login.html',{'user_input':user_input})
+	return render(request, 'index.html')
 
 def login(request):
     return render(request, 'login.html',{'user_input':""})
